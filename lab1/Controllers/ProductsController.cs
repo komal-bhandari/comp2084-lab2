115157314bbd085c1e538980a8bcdef5614d892b
komal-bhandari <komalbhandari216@gmail.com>
2018-09-24 02:04:39 -0400

    commiting lab2

---
+++ lab1/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Products/details
+        public ActionResult details(string ProductName)
+        {
+            ViewBag.ProductName = ProductName;
+            return View();
+        }
+    }
+}
